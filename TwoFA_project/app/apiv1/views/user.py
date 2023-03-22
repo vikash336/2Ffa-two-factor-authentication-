b1606ad09cf544717a700255d0d7c9926f756622
Varun jain <deepython05@gmail.com>
2023-03-22 11:13:19 +0530

    email

--- TwoFA_project/app/apiv1/views/user.py
+++ TwoFA_project/app/apiv1/views/user.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from ...models import UserAccount, TwoFaToken
 from ...serializers import MyUser_serializer, Two_fac_serializer
 from rest_framework.decorators import APIView
@@ -7,33 +6,59 @@ from rest_framework.exceptions import AuthenticationFailed
 from django.contrib.auth import authenticate
 import json
 from django.core.mail import send_mail
-from django.shortcuts import render
-
-
+import random as r
+from django.utils.crypto import get_random_string
+from django.shortcuts import get_object_or_404
+from rest_framework.status import HTTP_200_OK
 class send_otp(APIView):
 
-    def post(self, requset):
-        email=requset.data['email']
-        password=requset.data['password']
+    def post(self, request):
+        email=request.data['email']
+        password=request.data['password']
 
         verify_email=UserAccount.objects.filter(email=email).first()
-        # user_data = authenticate(email=email, password=password)
+        if verify_email is None:
+            raise AuthenticationFailed({
+                "Status": "Email Not Registered"
+            })
 
         if verify_email.check_password(password) is False:
             raise AuthenticationFailed({
                 "Status": "Incorrect Password"
             })
+        user_tokens = TwoFaToken.objects.filter(user=verify_email)
+        if user_tokens.exists():
+            user_tokens.delete()
+        token = TwoFaToken.objects.create(user=verify_email,
+                                          token=get_random_string(
+                                              length=6,
+                                              allowed_chars="0123456789"))
 
-        user_tokens = TwoFaToken.objects.filter(user=verify_email).first()
-        
-        subject = 'Subject of the Email'
-        message = 'Body of the Email'
+        subject = 'OTP for 2FA'
+        message = token.token
         from_email = 'vikashgusain1999@gmail.com'
         recipient_list = ['vikash.gusain@bonamisoftware.com',]
         send_mail(subject, message, from_email, recipient_list, fail_silently=False)
 
 
-
         return Response({
             'detail':'2fa code has been sent to your inbox'
-            })
+            },status=HTTP_200_OK)
+
+class VerifyTwoFactorAPIView(APIView):
+    def post(self, request):
+        email=request.data['email']
+        otp=request.data['otp']
+
+        print("******")
+        verify_email=UserAccount.objects.filter(email=email).first()
+        # user_token=TwoFaToken.objects.get(user=user,token=data['token'])
+        user_token = get_object_or_404(TwoFaToken,
+                                       user=verify_email,
+                                       token=otp)
+        print(user_token)
+        user_token.delete()
+        return Response({
+            'detail':'Accepted'
+
+        },status=HTTP_200_OK)
--- test.py
+++ test.py
@@ -52,47 +52,11 @@
 # Python program showing
 # abstract base class work
 
-from abc import ABC, abstractmethod
 
-class Polygon(ABC):
-	
-	@abstractmethod
-	def noofsides(self):
-		pass
+import random as r
 
-class Triangle(Polygon):
-
-	# overriding abstract method
-	def noofsides(self):
-		print("I have 3 sides")
-
-class Pentagon(Polygon):
-
-	# overriding abstract method
-	def noofsides(self):
-		print("I have 5 sides")
-
-class Hexagon(Polygon):
-
-	# overriding abstract method
-	def noofsides(self):
-		print("I have 6 sides")
-
-class Quadrilateral(Polygon):
-
-	# overriding abstract method
-	def noofsides(self):
-		print("I have 4 sides")
-
-# Driver code
-R = Polygon()
-R.noofsides()
-
-K = Quadrilateral()
-K.noofsides()
-
-R = Pentagon()
-R.noofsides()
-
-K = Hexagon()
-K.noofsides()
+otp=""
+for i in range(4):
+    otp+=str(r.randint(1,9))
+print ("Your One Time Password is ")
+print (otp)
